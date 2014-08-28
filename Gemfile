a4a8e816c4389b8ed8e381594c1e9ea682b47712
Jimmy <jimwong1023@gmail.com>
2014-08-28 13:44:27 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.1'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'pry-rails'
+gem 'rails_12factor'
+gem 'bcrypt-ruby', '~> 3.1.2'
+
+group :test do
+  gem 'factory_girl_rails'
+  gem 'capybara', '1.1.2'
+  gem 'database_cleaner', '0.7.0'
+  gem 'shoulda-matchers'
+  gem 'selenium-webdriver'
+  gem 'faker'
+  gem 'launchy'
+end
+
+group :development, :test do
+  gem 'rspec-rails', '~> 2.0'
+  gem 'simplecov', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DungeonMaze::Application.config.secret_key_base = '2dbf23854e59714bb9e0bff825bb1b36b1715a190f712f4ce3eee1852239460b197356257d0b2d9d7d22ba7e90ec08c2e7790b32585d496ba9198b75b3dfe1d6'
